326e6a3f0de62d0f9697aa9594d165a0951118af
SwapnilBhoyar <swapnilbhoyar95@gmail.com>
2021-04-29 16:49:15 +0530

    Added line compaire

--- lineComparisionOOP.java
+++ lineComparisionOOP.java
@@ -1,4 +1,25 @@
 import java.lang.Math;
+class lineFunction{
+
+        int getLineValue(){
+                int value = (int)(Math.random() * 10);
+                return value;
+        }
+
+        double getLineLength(int x1,int x2, int y1,int y2){
+                double x = Math.pow((x2 - x1), 2);
+                double y = Math.pow((y2 - y1), 2);
+                return Math.sqrt(x + y);
+        }
+        boolean isEqual(double length , double length1){
+                if(String.valueOf(length).equals(String.valueOf(length1))){
+                        return true;
+                }
+                else{
+                        return false;
+                }
+        }
+}
 public class lineComparisionOOP{
 	public static void main(String args[]){
 		System.out.println("Wellcome To Line Comparison Computation Program On Master Branch");
@@ -7,21 +28,18 @@ public class lineComparisionOOP{
 		int x2 = lineFunctionObject.getLineValue();
 		int y1 = lineFunctionObject.getLineValue();
 		int y2 = lineFunctionObject.getLineValue();
-		double length = lineFunctionObject.getLineLength(x1,x2,y1,y2);
-		System.out.println("length:" + length);
+		int x11 = lineFunctionObject.getLineValue();
+                int x12 = lineFunctionObject.getLineValue();
+                int y11 = lineFunctionObject.getLineValue();
+                int y12 = lineFunctionObject.getLineValue();
+		double length = lineFunctionObject.getLineLength(x1 , x2 , y1 , y2);
+		double length1 = lineFunctionObject.getLineLength(x11 , x12 , y11 , y12);
+		boolean isEqual = lineFunctionObject.isEqual(length , length1);
+		if(isEqual == true){
+			System.out.println("Lines are equal!");
+		}
+		else{
+			System.out.println("Lines are not equal!");
+		}
 	}
-}
-class lineFunction{
-
-	int getLineValue(){
-        	int value = (int)(Math.random() * 100);
-        	return value;
-        }
-
-	double getLineLength(int x1,int x2, int y1,int y2){
-		double x = Math.pow((x2 - x1), 2);
-		double y = Math.pow((y2 - y1), 2);
-		return Math.sqrt(x + y);
-	}
-
 }
