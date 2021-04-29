3c77bd1e9db108ab1c4690b021ba917012786717
SwapnilBhoyar <swapnilbhoyar95@gmail.com>
2021-04-29 17:26:13 +0530

    Added equal and compaire

--- lineComparisionOOP.java
+++ lineComparisionOOP.java
@@ -11,6 +11,14 @@ class lineFunction{
                 double y = Math.pow((y2 - y1), 2);
                 return Math.sqrt(x + y);
         }
+        boolean isEqual(double length , double length1){
+                if(String.valueOf(length).equals(String.valueOf(length1))){
+                        return true;
+                }
+                else{
+                        return false;
+                }
+        }
 	int compaire(double length , double length1){
 		int result = Double.valueOf(length).compareTo(Double.valueOf(length1));
 		return result;
@@ -30,15 +38,19 @@ public class lineComparisionOOP{
                 int y12 = lineFunctionObject.getLineValue();
 		double length = lineFunctionObject.getLineLength(x1 , x2 , y1 , y2);
 		double length1 = lineFunctionObject.getLineLength(x11 , x12 , y11 , y12);
+		boolean isEqual = lineFunctionObject.isEqual(length , length1);
+		if(isEqual == true){
+			System.out.println("Lines are equal!");
+		}
+		else{
+			System.out.println("Lines are not equal!");
+		}
 		int compaire = lineFunctionObject.compaire(length , length1);
 		if(compaire >= 1){
 			System.out.println("line1 is greater than line2");
 		}
 		else if(compaire < 0){
 			System.out.println("line1 is less than line2");
-		}
-		else{
-			System.out.println("Lines are equal!");
 		}
 	}
 }
