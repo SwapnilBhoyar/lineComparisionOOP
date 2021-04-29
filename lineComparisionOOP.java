884c009f76ced3ef91493d4726891a35a4298867
SwapnilBhoyar <swapnilbhoyar95@gmail.com>
2021-04-29 14:27:25 +0530

    Added dispaly message

---
+++ lineComparisionOOP.java
@@ -0,0 +1,7 @@
+import java.io.*;
+import java.lang.Math;
+public class lineComparisionOOP{
+	public static void main(String args[]){
+		System.out.println("Wellcome To Line Comparison Computation Program On Master Branch");
+	}
+}
