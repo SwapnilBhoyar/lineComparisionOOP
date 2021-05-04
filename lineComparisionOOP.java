b6b63f48c5562e60c8f506c953be1a14135bcf8c
SwapnilBhoyar <swapnilbhoyar95@gmail.com>
2021-05-04 14:19:46 +0530

    Refactored

--- lineComparisionOOP.java
+++ lineComparisionOOP.java
@@ -1,56 +1,65 @@
 import java.lang.Math;
-class lineFunction{
+class lineFunction {
 
-        int getLineValue(){
-                int value = (int)(Math.random() * 10);
-                return value;
-        }
+    int getLineValue() {
+        int value = (int)(Math.random() * 10);
+        return value;
+    }
 
-        double getLineLength(int x1,int x2, int y1,int y2){
-                double x = Math.pow((x2 - x1), 2);
-                double y = Math.pow((y2 - y1), 2);
-                return Math.sqrt(x + y);
+    double getLineLength(int x1,int x2, int y1,int y2) {
+        double x = Math.pow((x2 - x1), 2);
+        double y = Math.pow((y2 - y1), 2);
+        return Math.sqrt(x + y);
+    }
+
+    boolean isEqual(double length , double length1) {
+        if(String.valueOf(length).equals(String.valueOf(length1))) {
+            return true;
         }
-        boolean isEqual(double length , double length1){
-                if(String.valueOf(length).equals(String.valueOf(length1))){
-                        return true;
-                }
-                else{
-                        return false;
-                }
+        else {
+            return false;
         }
-	int compaire(double length , double length1){
-		int result = Double.valueOf(length).compareTo(Double.valueOf(length1));
-		return result;
-	}
+    }
+	
+    int compaire(double length , double length1) {
+        int result = Double.valueOf(length).compareTo(Double.valueOf(length1));
+        return result;
+    }
 }
-public class lineComparisionOOP{
-	public static void main(String args[]){
-		System.out.println("Wellcome To Line Comparison Computation Program On Master Branch");
-		lineFunction lineFunctionObject = new lineFunction();
-		int x1 = lineFunctionObject.getLineValue();
-		int x2 = lineFunctionObject.getLineValue();
-		int y1 = lineFunctionObject.getLineValue();
-		int y2 = lineFunctionObject.getLineValue();
-		int x11 = lineFunctionObject.getLineValue();
-                int x12 = lineFunctionObject.getLineValue();
-                int y11 = lineFunctionObject.getLineValue();
-                int y12 = lineFunctionObject.getLineValue();
-		double length = lineFunctionObject.getLineLength(x1 , x2 , y1 , y2);
-		double length1 = lineFunctionObject.getLineLength(x11 , x12 , y11 , y12);
-		boolean isEqual = lineFunctionObject.isEqual(length , length1);
-		if(isEqual == true){
-			System.out.println("Lines are equal!");
-		}
-		else{
-			System.out.println("Lines are not equal!");
-		}
-		int compaire = lineFunctionObject.compaire(length , length1);
-		if(compaire >= 1){
-			System.out.println("line1 is greater than line2");
-		}
-		else if(compaire < 0){
-			System.out.println("line1 is less than line2");
-		}
-	}
+
+public class lineComparisionOOP {
+
+    public static void main(String args[]) {
+        System.out.println("Wellcome To Line Comparison Computation Program On Master Branch");
+        lineFunction lineFunctionObject = new lineFunction();
+        
+        int x1 = lineFunctionObject.getLineValue();
+        int x2 = lineFunctionObject.getLineValue();
+        int y1 = lineFunctionObject.getLineValue();
+        int y2 = lineFunctionObject.getLineValue();
+        
+        int x11 = lineFunctionObject.getLineValue();
+        int x12 = lineFunctionObject.getLineValue();
+        int y11 = lineFunctionObject.getLineValue();
+        int y12 = lineFunctionObject.getLineValue();
+        
+        double length = lineFunctionObject.getLineLength(x1 , x2 , y1 , y2);
+        double length1 = lineFunctionObject.getLineLength(x11 , x12 , y11 , y12);
+        boolean isEqual = lineFunctionObject.isEqual(length , length1);
+        
+        if(isEqual == true) {
+            System.out.println("Lines are equal!");
+        }
+        else {
+            System.out.println("Lines are not equal!");
+        }
+
+        int compaire = lineFunctionObject.compaire(length , length1);
+        if(compaire >= 1) {
+            System.out.println("line1 is greater than line2");
+        }
+        else if(compaire < 0) {
+            System.out.println("line1 is less than line2");
+        }
+    }
 }
